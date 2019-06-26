37e217f1a8ad9beba13866a41a9ec64519fab14d
Joseph Koon <josephandrewkoon@gmail.com>
2019-06-25 18:14:55 -0700

    Updates

---
+++ precache-manifest.80eca810f63a40667cd2f9de75c4f4a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424f894823b7e391c6804d340d480fa6",
+    "url": "/shopmate/index.html"
+  },
+  {
+    "revision": "fd29b6c82c65a4e9e032",
+    "url": "/shopmate/static/css/main.f6c025f8.chunk.css"
+  },
+  {
+    "revision": "ba70c063c6d6269a487a",
+    "url": "/shopmate/static/js/2.2a3dc0ca.chunk.js"
+  },
+  {
+    "revision": "fd29b6c82c65a4e9e032",
+    "url": "/shopmate/static/js/main.f0fae45e.chunk.js"
+  },
+  {
+    "revision": "f89ab9f7d8da43b71c67",
+    "url": "/shopmate/static/js/runtime~main.03f445c5.js"
+  },
+  {
+    "revision": "294e063695792e06a6ac35d1127509ab",
+    "url": "/shopmate/static/media/modal4.294e0636.png"
+  },
+  {
+    "revision": "502184ca941d6fa1e9f75118b67896a8",
+    "url": "/shopmate/static/media/shoe2.502184ca.png"
+  }
+]);
